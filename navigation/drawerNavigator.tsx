6c98b2b1320757c526a4dbd425039209b7b20d80
Ninny <jordansamcooke@gmail.com>
2023-06-20 12:34:10 +1000

    Working - Updated npx expo

--- navigation/drawerNavigator.tsx
+++ navigation/drawerNavigator.tsx
@@ -1,20 +1,24 @@
 import "react-native-gesture-handler";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Button } from "react-native";
+
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
   DrawerItem,
   DrawerItemList,
 } from "@react-navigation/drawer";
+
 import * as React from "react";
+
+import LoginScreen from './Screen/LoginScreen';
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigation() {
   return (
-    <Drawer.Navigator initialRouteName="loginScreen">
-      <Stack.Screen name="loginScreen" component={loginScreen} />
+    <Drawer.Navigator initialRouteName="LoginScreen">
+      <Stack.Screen name="Login" component={LoginScreen} />
     </Drawer.Navigator>
   );
 }
--- screen/loginScreen.tsx
+++ screen/loginScreen.tsx
@@ -1,10 +1,61 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Picker } from "react-native";
 
-export default function LoginScreen() {
+const LoginScreen = () => {
+  const [selectedCompany, setSelectedCompany] = useState("Tronox");
 
-    return (
+  const handleCompanyChange = (itemValue) => {
+    setSelectedCompany(itemValue);
+  };
 
-    );
-}
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Login</Text>
+      <View style={styles.dropdownContainer}>
+        <Text style={styles.label}>Company:</Text>
+        <Picker
+          style={styles.dropdown}
+          selectedValue={selectedCompany}
+          onValueChange={handleCompanyChange}
+        >
+          <Picker.Item label="Tronox" value="Tronox" />
+          <Picker.Item label="Example 2" value="Example 2" />
+        </Picker>
+      </View>
+      {/* Add other login components like input fields and buttons here */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f2f2f2",
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  dropdownContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 18,
+    marginRight: 10,
+  },
+  dropdown: {
+    flex: 1,
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 4,
+  },
+});
+
+export default LoginScreen;
